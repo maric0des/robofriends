8d5385b80d5eef29c752b0224e53dfb5da0959b9
maric0des <maricodes.dev@gmail.com>
2020-07-04 19:09:12 +0900

    Updates

---
+++ precache-manifest.ae35fc16f773e9ad5498f3356ea98509.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557949ec6af17e9fefbaa1b3f7a57524",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "94957e2274d8d5e0cf24",
+    "url": "/robofriends/static/css/main.5f2d9fbd.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94957e2274d8d5e0cf24",
+    "url": "/robofriends/static/js/main.83f04fa5.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "aa67782930dad920cd20fe029a6ac9ce",
+    "url": "/robofriends/static/media/audiowide-regular-webfont.aa677829.woff"
+  }
+]);
